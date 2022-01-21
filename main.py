bcfd7b717c22494b395f324cdd21d2de3455d23c
Mitlasóczki Bence <mitlabence111@gmail.com>
2022-01-21 18:01:10 +0100

    Test changes (committing, pushing)

--- main.py
+++ main.py
@@ -2,10 +2,8 @@
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-import typing
-String = str
 
-def print_hi(name: String):
+def print_hi(name: str):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
