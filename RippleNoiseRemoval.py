6738c56ce75d3325b93aa96d74bd51d9865ebf3a
Mitlasóczki Bence <mitlabence111@gmail.com>
2022-05-18 13:06:11 +0200

    Added finalized test script to RNR class

--- RippleNoiseRemoval.py
+++ RippleNoiseRemoval.py
@@ -3,6 +3,7 @@ import numpy as np
 from matplotlib import pyplot as plt
 from nd2_to_caiman import np_arr_from_nd2
 from pathos.multiprocessing import ProcessingPool as Pool
+from pathos.helpers import cpu_count
 
 """
 # optional: show original figure of which plot is made
@@ -10,79 +11,106 @@ fig = plt.figure(figsize=(18,18))
 plt.pcolormesh(nd2_data[0])
 plt.show()
 """
+# TODO: write assert() tests!
+# TODO: write test on test data to compare parallel and single thread RNR,
+#       also compare with Matlab RNR!
+# TODO: make it work for 2-channel recordings too!
 
 
-def rnr_frame(frame, win, amplitude_threshold):  # endx and endy should be
-    freq_image = np.fft.fftshift(np.fft.fft2(frame))  # make FFT
-    # get log amplitude to detect spikes in fft
-    ampl_image = np.log(np.abs(freq_image))
-    end_x = ampl_image.shape[0]
-    end_y = ampl_image.shape[1]
-    bright_spikes = ampl_image > amplitude_threshold
-    bright_spikes[round(end_x/2-win):round(end_x/2+win),
-                  round(end_y/2-win):round(end_y/2+win)] = 0
-    freq_image[bright_spikes] = 0
-    filt_image = np.fft.ifft2(freq_image)
-    return np.abs(filt_image)
+class RNR():
+    def __init__(self, win, amplitude_threshold, n_threads):
+        self.win = win
+        self.amplitude_threshold = amplitude_threshold
+        self.end_x = 0
+        self.end_y = 0
+        self.n_frames = 0
+        self.nd2_data = None  # raw data
+        self.rnr_data = None  # contains RNR-processed data
+        # TODO: if n_threads = 1, only do rnr_singlethread. Otherwise make sure computer has enough threads, perform parallel
+        self.n_threads = n_threads
+        assert(self.n_threads <= cpu_count())
 
+    def open_recording(self, nd2_fpath):
+        self.nd2_data = np_arr_from_nd2(nd2_fpath)
+        self.rnr_data = np.empty(self.nd2_data.shape, dtype=np.float64)
+        self.n_frames = self.nd2_data.shape[0]
+        self.end_x = self.nd2_data.shape[1]
+        self.end_y = self.nd2_data.shape[2]
+        print(
+            f"Opened recording {self.end_x}x{self.end_y}, {self.n_frames} frames. Initialized empty results array.")
 
-def plot_rnr(frame, win, amplitude_threshold):  # ex. frame = nd2_data[0, :, :]
-    freq_matrix = np.fft.fftshift(np.fft.fft2(frame))
-    amplitude_image = np.log(np.abs(freq_matrix))
-    bright_spikes = amplitude_image > amplitude_threshold  # default amplitude threshold
-    rectangle_filter_boundary = np.zeros(amplitude_image.shape)
-    filtered_spikes = np.copy(bright_spikes)
-    end_y = amplitude_image.shape[0]  # todo: maybe switched?
-    end_x = amplitude_image.shape[1]
+    def rnr_frame(self, frame):  # endx and endy should be
+        freq_image = np.fft.fftshift(np.fft.fft2(
+            frame))  # make FFT
+        # get log amplitude to detect spikes in fft
+        ampl_image = np.log(np.abs(freq_image))
+        bright_spikes = ampl_image > self.amplitude_threshold
+        bright_spikes[round(self.end_x/2-self.win):round(self.end_x/2+self.win),
+                      round(self.end_y/2-self.win):round(self.end_y/2+self.win)] = 0
+        freq_image[bright_spikes] = 0
+        filt_image = np.fft.ifft2(freq_image)
+        return np.abs(filt_image)
 
-    # plot rectangle that will be filtered out
-    rectangle_filter_boundary[round(
-        end_x/2 - win):round(end_x/2 + win), round(end_y/2 - win)] = 1
-    rectangle_filter_boundary[round(
-        end_x/2 - win):round(end_x/2 + win), round(end_y/2 + win)] = 1
-    rectangle_filter_boundary[round(end_x/2 - win),
-                              round(end_y/2 - win):round(end_y/2 + win)] = 1
-    rectangle_filter_boundary[round(end_x/2 + win),
-                              round(end_y/2 - win):round(end_y/2 + win)] = 1
-    filtered_spikes[round(end_x/2-win):round(end_x/2+win),
-                    round(end_y/2-win):round(end_y/2+win)] = 0
-    # filter out whole fft spectrum, show raw and filtered
-    freq_filtered = np.copy(freq_matrix)
-    freq_filtered[bright_spikes] = 0.0
+    # ex. frame = nd2_data[0, :, :]
+    def plot_rnr(self, i_frame: int = 0):
+        assert(self.nd2_data is not None)
+        freq_matrix = np.fft.fftshift(np.fft.fft2(self.nd2_data[i_frame]))
+        amplitude_image = np.log(np.abs(freq_matrix))
+        # default amplitude threshold
+        bright_spikes = amplitude_image > self.amplitude_threshold
+        rectangle_filter_boundary = np.zeros(amplitude_image.shape)
+        filtered_spikes = np.copy(bright_spikes)
+        end_y = amplitude_image.shape[0]  # todo: maybe switched?
+        end_x = amplitude_image.shape[1]
 
-    filtered_image = np.abs(np.fft.ifft2(freq_filtered))
+        # plot rectangle that will be filtered out
+        rectangle_filter_boundary[round(
+            end_x/2 - win):round(self.end_x/2 + self.win), round(self.end_y/2 - self.win)] = 1
+        rectangle_filter_boundary[round(
+            end_x/2 - win):round(self.end_x/2 + self.win), round(self.end_y/2 + self.win)] = 1
+        rectangle_filter_boundary[round(self.end_x/2 - self.win),
+                                  round(self.end_y/2 - self.win):round(self.end_y/2 + self.win)] = 1
+        rectangle_filter_boundary[round(self.end_x/2 + self.win),
+                                  round(self.end_y/2 - self.win):round(self.end_y/2 + self.win)] = 1
+        filtered_spikes[round(self.end_x/2-self.win):round(self.end_x/2+self.win),
+                        round(self.end_y/2-self.win):round(self.end_y/2+self.win)] = 0
+        # filter out whole fft spectrum, show raw and filtered
+        freq_filtered = np.copy(freq_matrix)
+        freq_filtered[bright_spikes] = 0.0
 
-    # plot various data
-    fig, axs = plt.subplots(3, 3, figsize=(20, 20))
-    axs[0, 0].pcolormesh(amplitude_image)
-    axs[0, 1].pcolormesh(bright_spikes + rectangle_filter_boundary)
-    axs[0, 2].pcolormesh(np.abs(freq_filtered))
-    axs[1, 0].pcolormesh(filtered_spikes)
-    for i_row in range(len(amplitude_image)):
-        axs[1, 1].plot(amplitude_image[i_row, :])
-    axs[1, 1].axhline(y=amplitude_threshold, color='r', linestyle='-')
-    # axs[0,0].colorbar()
-    axs[1, 2].pcolormesh(np.log(np.abs(freq_matrix - freq_filtered)))
-    # plot original and raw
-    axs[2, 0].pcolormesh(frame)
-    axs[2, 1].pcolormesh(filtered_image)
-    plt.show()
+        filtered_image = np.abs(np.fft.ifft2(freq_filtered))
 
+        # plot various data
+        fig, axs = plt.subplots(3, 3, figsize=(20, 20))
+        axs[0, 0].pcolormesh(amplitude_image)
+        axs[0, 1].pcolormesh(bright_spikes + rectangle_filter_boundary)
+        axs[0, 2].pcolormesh(np.abs(freq_filtered))
+        axs[1, 0].pcolormesh(filtered_spikes)
+        for i_row in range(len(amplitude_image)):
+            axs[1, 1].plot(amplitude_image[i_row, :])
+        axs[1, 1].axhline(y=self.amplitude_threshold, color='r', linestyle='-')
+        # axs[0,0].colorbar()
+        axs[1, 2].pcolormesh(np.log(np.abs(freq_matrix - freq_filtered)))
+        # plot original and raw
+        axs[2, 0].pcolormesh(self.nd2_data[i_frame])
+        axs[2, 1].pcolormesh(filtered_image)
+        plt.show()
 
-def rnr():
-    nd2_fpath = 'D:/PhD/Data/T386_MatlabTest/T386_20211202_green.nd2'
-    win = 40
-    amplitude_threshold = 10.8
+    def rnr_singlethread(self):
+        for i_frame in range(self.n_frames):
+            self.rnr_data[i_frame] = self.rnr_frame(
+                self.nd2_data[i_frame, :, :])
+        print("RNR completed.")
+        return self.rnr_data
 
-    nd2_data = np_arr_from_nd2(nd2_fpath)
-
-    filtered_data = np.zeros(nd2_data.shape)
-    for i_frame in range(nd2_data.shape[0]):
-        filtered_data[i_frame] = rnr_frame(
-            nd2_data[i_frame, :, :], win, amplitude_threshold)
-    return filtered_data
-
-
-
-	
-	
+    def rnr_parallel(self):
+        # TODO: there are some issues with starting the pool again: https://github.com/uqfoundation/pathos/issues/111
+        # FIXME: Running this method again results in ValueError: Pool not running. Possible solution: try ... except ValueError: pool.restart()
+        # TODO: parallel seems much slower!
+        p = Pool(self.n_threads)
+        res_list = p.map(self.rnr_frame, self.nd2_data)
+        for i_frame, frame in enumerate(res_list):
+            self.rnr_data[i_frame] = frame
+        print(f"RNR with {self.n_threads} threads completed.")
+        p.close()
+        return self.rnr_data
