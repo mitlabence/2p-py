1a3769e763bf7f851abbbb2ba1861396ede73227
Bence Mitlasoczki <mitlabence111@gmail.com>
2023-03-27 11:09:44 +0200

    Added multithread RNR

--- RippleNoiseRemoval.py
+++ RippleNoiseRemoval.py
@@ -2,12 +2,16 @@ import numpy as np
 from matplotlib import pyplot as plt
 from nd2_to_caiman import np_arr_from_nd2
 from typing import Tuple
+import multiprocess as mp  # multiprocessing does not work with IPython. Use fork instead.
+
 """
 # optional: show original figure of which plot is made
 fig = plt.figure(figsize=(18,18))
 plt.pcolormesh(nd2_data[0])
 plt.show()
 """
+
+
 # TODO: write assert() tests!
 # TODO: write test on test data to compare parallel and single thread RNR,
 #       also compare with Matlab RNR!
@@ -28,7 +32,7 @@ class RNR():
         self.n_threads = n_threads
         # assert(self.n_threads <= cpu_count())
 
-    def open_recording(self, nd2_fpath, begin_end_frames: Tuple[int, int]=None):
+    def open_recording(self, nd2_fpath, begin_end_frames: Tuple[int, int] = None):
         self.nd2_data = np_arr_from_nd2(nd2_fpath, begin_end_frames)
         self.rnr_data = np.empty(self.nd2_data.shape, dtype=np.float64)
         self.n_frames = self.nd2_data.shape[0]
@@ -44,15 +48,15 @@ class RNR():
         # D:\Codebase\2p-py\RippleNoiseRemoval.py:46: RuntimeWarning: divide by zero encountered in log ampl_image = np.log(np.abs(freq_image))
         ampl_image = np.log(np.abs(freq_image))
         bright_spikes = ampl_image > self.amplitude_threshold
-        bright_spikes[round(self.end_x/2-self.win):round(self.end_x/2+self.win),
-                      round(self.end_y/2-self.win):round(self.end_y/2+self.win)] = 0
+        bright_spikes[round(self.end_x / 2 - self.win):round(self.end_x / 2 + self.win),
+        round(self.end_y / 2 - self.win):round(self.end_y / 2 + self.win)] = 0
         freq_image[bright_spikes] = 0
         filt_image = np.fft.ifft2(freq_image)
         return np.abs(filt_image)
 
     # ex. frame = nd2_data[0, :, :]
     def plot_rnr(self, i_frame: int = 0):
-        assert(self.nd2_data is not None)
+        assert (self.nd2_data is not None)
         freq_matrix = np.fft.fftshift(np.fft.fft2(self.nd2_data[i_frame]))
         amplitude_image = np.log(np.abs(freq_matrix))
         # default amplitude threshold
@@ -64,15 +68,15 @@ class RNR():
 
         # plot rectangle that will be filtered out
         rectangle_filter_boundary[round(
-            end_x/2 - win):round(self.end_x/2 + self.win), round(self.end_y/2 - self.win)] = 1
+            end_x / 2 - win):round(self.end_x / 2 + self.win), round(self.end_y / 2 - self.win)] = 1
         rectangle_filter_boundary[round(
-            end_x/2 - win):round(self.end_x/2 + self.win), round(self.end_y/2 + self.win)] = 1
-        rectangle_filter_boundary[round(self.end_x/2 - self.win),
-                                  round(self.end_y/2 - self.win):round(self.end_y/2 + self.win)] = 1
-        rectangle_filter_boundary[round(self.end_x/2 + self.win),
-                                  round(self.end_y/2 - self.win):round(self.end_y/2 + self.win)] = 1
-        filtered_spikes[round(self.end_x/2-self.win):round(self.end_x/2+self.win),
-                        round(self.end_y/2-self.win):round(self.end_y/2+self.win)] = 0
+            end_x / 2 - win):round(self.end_x / 2 + self.win), round(self.end_y / 2 + self.win)] = 1
+        rectangle_filter_boundary[round(self.end_x / 2 - self.win),
+        round(self.end_y / 2 - self.win):round(self.end_y / 2 + self.win)] = 1
+        rectangle_filter_boundary[round(self.end_x / 2 + self.win),
+        round(self.end_y / 2 - self.win):round(self.end_y / 2 + self.win)] = 1
+        filtered_spikes[round(self.end_x / 2 - self.win):round(self.end_x / 2 + self.win),
+        round(self.end_y / 2 - self.win):round(self.end_y / 2 + self.win)] = 0
         # filter out whole fft spectrum, show raw and filtered
         freq_filtered = np.copy(freq_matrix)
         freq_filtered[bright_spikes] = 0.0
@@ -102,14 +106,32 @@ class RNR():
         print("RNR completed.")
         return self.rnr_data
 
-#    def rnr_parallel(self):
-#        # TODO: there are some issues with starting the pool again: https://github.com/uqfoundation/pathos/issues/111
-#        # FIXME: Running this method again results in ValueError: Pool not running. Possible solution: try ... except ValueError: pool.restart()
-#        # TODO: parallel seems much slower!
-#        p = Pool(self.n_threads)
-#        res_list = p.map(self.rnr_frame, self.nd2_data)
-#        for i_frame, frame in enumerate(res_list):
-#            self.rnr_data[i_frame] = frame
-#        print(f"RNR with {self.n_threads} threads completed.")
-#        p.close()
-#        return self.rnr_data
+    def rnr_multithread(self, n_processes: int = 2):
+        assert n_processes <= mp.cpu_count()
+        from functools import partial
+        def rnr_frame_process(frame, end_x, end_y, win, amplitude_threshold):
+            # Maybe possible with starmap, setting range so it includes a single value for each parameter?
+            import numpy as np
+
+            freq_image = np.fft.fftshift(np.fft.fft2(
+                frame))  # make FFT
+            # get log amplitude to detect spikes in fft
+            # D:\Codebase\2p-py\RippleNoiseRemoval.py:46: RuntimeWarning: divide by zero encountered in log ampl_image = np.log(np.abs(freq_image))
+            ampl_image = np.log(np.abs(freq_image))
+            bright_spikes = ampl_image > amplitude_threshold
+            bright_spikes[round(end_x / 2 - win):round(end_x / 2 + win),
+            round(end_y / 2 - win):round(end_y / 2 + win)] = 0
+            freq_image[bright_spikes] = 0
+            filt_image = np.fft.ifft2(freq_image)
+            return np.abs(filt_image)
+
+        partial_func = partial(rnr_frame_process, end_x=self.end_x, end_y=self.end_y, win=self.win,
+                               amplitude_threshold=self.amplitude_threshold)
+
+        result_arr = np.zeros(self.rnr_data.shape, dtype=self.rnr_data.dtype)
+
+        with mp.Pool(processes=n_processes) as p:
+            processed_data = p.map(partial_func, self.nd2_data[range(self.n_frames)]) # list of np.array
+            for i_frame in range(self.n_frames):
+                result_arr[i_frame] = processed_data[i_frame]
+        return result_arr
